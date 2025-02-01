a6424e07c683b0f51211f161fedb0d1700baecae
Kaushal1100 <jd@gmail.com>
2025-02-01 13:04:20 -0500

    EMS-First Commit: Project Creation

---
+++ emsspringbackend/src/main/java/com/example/emsspringbackend/EmsspringbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.emsspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsspringbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsspringbackendApplication.class, args);
+	}
+
+}
