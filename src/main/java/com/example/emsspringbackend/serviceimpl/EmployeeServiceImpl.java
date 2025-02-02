45199174fc97d88f5b612bb73471e1d23ec2e8d9
Kaushal1100 <jd@gmail.com>
2025-02-01 21:54:50 -0500

    EMS-5th: Exception and GetByID

--- src/main/java/com/example/emsspringbackend/serviceimpl/EmployeeServiceImpl.java
+++ src/main/java/com/example/emsspringbackend/serviceimpl/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.emsspringbackend.dto.EmployeeDto;
 import com.example.emsspringbackend.entity.Employee;
+import com.example.emsspringbackend.exception.ResourceNotFoundException;
 import com.example.emsspringbackend.mapper.EmployeeMapper;
 import com.example.emsspringbackend.repository.EmployeeRepository;
 import com.example.emsspringbackend.service.EmployeeService;
@@ -19,6 +20,13 @@ public class EmployeeServiceImpl implements EmployeeService{
 		Employee employee=EmployeeMapper.mapToEmployee(employeeDto);
 		Employee savedEmployee=employeeRepository.save(employee);
 		return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+	}
+	@Override
+	public EmployeeDto getEmployeeById(Long employeeId) {
+		Employee employee=employeeRepository.findById(employeeId)
+			.orElseThrow(() -> 
+			new ResourceNotFoundException("Employee doesnt exist: "+employeeId));
+		return EmployeeMapper.mapToEmployeeDto(employee);
 	}
 
 }
